2263acaad2501376f641294ff52828441bd22f9e
Aliyeva Oydin <mdroid786@gmail.com>
2022-10-24 15:34:30 +0500

    model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class User {
+
+    private String userName;
+    private String surName;
+    private String firstName;
+    private String password;
+    private String email;
+    private String dataOfBirth;
+    private String gender;
+
+
+
+}
