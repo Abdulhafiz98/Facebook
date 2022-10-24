637e7d36204ea0d7f6afabff0ea79fcf6f6e2d81
Khojiakbar Abiyorov <hojakbarabiyorov@gmail.com>
2022-10-24 16:05:43 +0500

    pull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import model.Contact;
-
 
 /**
  * @author Xojiakbar Abiyerov
  */
 public class Main {
     public static void main(String[] args) {
-        Contact contact = new Contact();
-
 
     }
 }
