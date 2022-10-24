94384ad1f1c2312de4dacd36cda5c395ed6a689d
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2022-10-24 16:25:51 +0500

    List qoshildi

---
+++ src/main/java/dataBase/Lists.java
@@ -0,0 +1,11 @@
+package dataBase;
+
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Lists {
+    public static List<User> USERS_LIST = new ArrayList<>();
+
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,6 @@
 package model;
 
+import base.Base;
 import lombok.*;
 
 @AllArgsConstructor
