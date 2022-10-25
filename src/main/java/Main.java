1a8eb160d503cbf7d5f63e2ae131cd88cd9e865f
Khojiakbar Abiyorov <hojakbarabiyorov@gmail.com>
2022-10-25 12:13:23 +0500

    Contact o`zgartirildi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import model.Contact;
+
+import java.util.ConcurrentModificationException;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -1,15 +1,12 @@
 package model;
-
 import base.Base;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.extern.java.Log;
+import lombok.*;
 
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Contact  extends Base {
     private int userId;
@@ -17,20 +14,12 @@ public class Contact  extends Base {
     private String contactSurname;
     private String contactNumber;
 
-
-    public Contact(int userId, String contactName, String contactNumber, String contactSurname) {
-        super();
-        this.userId = userId;
-        this.contactSurname = contactSurname;
-        this.contactName = contactName;
-        this.contactNumber = contactNumber;
-    }
-
     @Override
     public String toString() {
-        return "\t\t *** Contacts ***" +"\n"+
+        return  "\t\t *** Contacts ***" +"\n"+
                 " User Id : " + userId +"\n"+
                 " Name : " + contactName + "\n" +
+                " Surname : " + contactSurname + "\n" +
                 " Number : " + contactNumber + "\n";
     }
 }
