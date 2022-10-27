fdf3a9d692987bddced3ab5f9c4c818242d612a8
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2022-10-27 09:44:55 +0500

    Group classes are added

--- src/main/java/serivice/BaseService.java
+++ src/main/java/serivice/BaseService.java
@@ -2,6 +2,9 @@ package serivice;
 
 public interface BaseService {
     boolean add(Object o);
+
+    boolean add(Object o, int groupId);
+
     Object getById(int id);
     boolean deleteById(int id);
 
